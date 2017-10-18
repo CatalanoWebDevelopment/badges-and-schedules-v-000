6571776afb7d8d1f2902766a09ecf57538a38274
Parker Catalano <catalano.work@gmail.com>
2017-10-18 20:09:32 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -20,7 +20,7 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-  assign_rooms(attendees).map do |x|
+  assign_rooms(attendees).each do |x|
     puts "#{x}"
   end
   puts assign_rooms(attendees)
